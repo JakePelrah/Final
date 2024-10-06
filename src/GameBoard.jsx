2ba94968573c8b23d040cdba2bcaadd6c3d9ce9f
Jacob Pelrah <pelrahjsp@gmail.com>
2024-10-06 00:42:42 -0400

    css

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -8,6 +8,10 @@ export default function GameBoard() {
     const { score } = useGame()
 
     const renderScore = score => [...Array(score).keys()].map(star => <div><img className="star" src="./star.svg"></img></div>)
+    const renderUsers = [...Array(18).keys()].map(user => <span className="player-badge">
+        MM
+    </span>
+    )
 
     return (
         <div id="gameboard" className="d-flex justify-content-between">
@@ -19,10 +23,22 @@ export default function GameBoard() {
             </div>
 
             <div id="launch-platform" className="d-flex gap-5">
-                <Rocket imgPath='rocket_01.svg' />
-                <Rocket imgPath='rocket_02.svg' />
+                <Rocket imgPath='rocket_01.svg' variation={0} />
+                <Rocket imgPath='rocket_02.svg' variation={1} />
             </div>
-            <div className="controls"></div>
+
+            <div className="controls d-flex align-items-center gap-5">
+               
+                <div className="d-flex justify-content-center flex-wrap gap-2">
+                    {renderUsers}
+                </div>
+               
+                <div className="divider m-5"></div>
+        
+                <div className="d-flex justify-content-center flex-wrap gap-2">
+                    {renderUsers}
+                </div>
+            </div>
 
             <div id="score=right" className="score">
                 <div className="">
